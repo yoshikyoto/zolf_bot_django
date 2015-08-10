29c485b9fb2c6c123ac48d53008778a214aec6f2
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-08-11 01:12:04 +0900

    LANGUAGE_CODEをjaに、TIME_ZONEをAsia/Tokyoにした

--- zolf_bot/settings.py
+++ zolf_bot/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
